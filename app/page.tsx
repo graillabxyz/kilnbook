5e5ffca50dfd1de7d9ffed2b62c02c243548e4f1
Grail Lab <graillab.xyz@gmail.com>
2026-07-16 18:28:22 +0800

    Render live feed dynamically

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { KilnbookWorkspace } from "./kilnbook-workspace";
 import { previewRepository } from "@/lib/services/kilnbook-repository";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const snapshot = await previewRepository.getWorkspaceSnapshot("anonymous");
 
